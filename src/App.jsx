7a950366e5cf504ef48cad7b024fdcebc1dc2062
Kerry <trankerry94@gmail.com>
2021-12-16 13:35:09 -0800

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+// import MovieCards from './movieComponent';
 
 export default class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			movieOption1: [],
-			movieOption2: []
-		};
+		this.state = { 
+            movieOption1: [],
+			movieOption2: [],
+			clicked: false
+         };
 		this.handleClick = this.handleClick.bind(this);
 	}
 
 	handleClick() {
-		Axios
-		.get(`https://imdb-api.com/en/API/MostPopularMovies/k_i5h79ogg`)
-		.then(
-			(res) => {
-				const num = Math.floor(Math.random() * 100);
-				const num2 = Math.floor(Math.random() * 100);
-				const movie1 = res.data.items[num];
-				const movie2 = res.data.items[num2];
-				this.setState({ movieOption1: movie1 });
-				this.setState({movieOption2: movie2});
-				console.log(movie1);
-				console.log(movie2);
-			}
-		)
+		Axios.get(
+			`https://imdb-api.com/en/API/MostPopularMovies/${process.env.REACT_APP_MY_API_KEY}`
+		).then((res) => {
+			const num = Math.floor(Math.random() * 100);
+			const num2 = Math.floor(Math.random() * 100);
+			const movie1 = res.data.items[num];
+			const movie2 = res.data.items[num2];
+			this.setState({ movieOption1: movie1 });
+			this.setState({ movieOption2: movie2 });
+			console.log(movie1);
+			console.log(movie2);
+		});
+		this.setState({clicked: true})
 	}
 
 	render() {
@@ -69,101 +70,7 @@ export default class App extends Component {
 								</div>
 							</div>
 						</div>
-
-
-						<div className='row mx-5 justify-content-between'>
-  <div className="card mb-3">
-  <div className="row no-gutters">
-    <div className="col-md-4">
-      <img src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.nerdspan.com%2Fwp-content%2Fuploads%2F2013%2F12%2Felf-movie-poster.jpg&f=1&nofb=1' alt="..." width='150px'/>
-    </div>
-    <div className="col-md-8">
-      <div className="card-body">
-        <h5 className="card-title">Elf</h5>
-        <p className="card-text">Release Year: 2003</p>
-        <p className="card-text">Director and Cast: Josh Gordon (dir.) Jason Bateman, Olivia Munn</p>
-        <p className="card-text"><small className="text-muted">Current Rank: 91</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div className="card mb-3">
-  <div className="row no-gutters">
-    <div className="col-md-4">
-      <img src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.nerdspan.com%2Fwp-content%2Fuploads%2F2013%2F12%2Felf-movie-poster.jpg&f=1&nofb=1' alt="..." width='150px'/>
-    </div>
-    <div className="col-md-8">
-      <div className="card-body">
-        <h5 className="card-title">Elf</h5>
-        <p className="card-text">Release Year: 2003</p>
-        <p className="card-text">Director and Cast: Josh Gordon (dir.) Jason Bateman, Olivia Munn</p>
-        <p className="card-text"><small className="text-muted">Current Rank: 91</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-						{/* <div className='row align-items-md-stretch'>
-							<div className='offset-md-1 col-md-5'>
-								<div className='h-100 p-4 rounded text-white bg-dark'>
-									<div className='card'>
-										<img
-											className='card-img-top'
-											alt='...'
-											src={this.state.movieOption1.image}
-										/>
-										<div className='card-body'>
-											<h5 className='card-title text-center fs-3'>
-												{this.state.movieOption1.title}
-											</h5>
-											<div className='mb-3 row'>
-												<label
-													for='staticEmail'
-													className='col-sm-3 col-form-label'>
-													Year: {this.state.movieOption1.year}
-												</label>
-											</div>
-											<div className='mb-3 row'>
-												<label
-													for='staticEmail'
-													className='col-sm-3 col-form-label'>
-													Rank: {this.state.movieOption1.rank}
-												</label>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className='col-md-5'>
-								<div className='h-100 p-4 rounded'>
-									<div className='card'>
-										<img
-											className='card-img-top'
-											alt='...'
-											src={this.state.movieOption2.image}
-										/>
-										<div className='card-body'>
-											<h5 className='card-title text-center fs-3'>
-												{this.state.movieOption2.title}
-											</h5>
-											<div className='mb-3 row'>
-												<label
-													className='col-sm-3 col-form-label'>
-													Year: {this.state.movieOption2.year}
-												</label>
-											</div>
-											<div className='mb-3 row'>
-												<label
-													className='col-sm-3 col-form-label'>
-													Rank: {this.state.movieOption2.rank}
-												</label>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div> */}
+						{/* {(this.state.clicked) ? <MovieCards movieOption1={this.state.movieOption1} movieOption2={this.state.movieOption2}/> : <div className='my-5 py-5'> </div>} */}
 					</div>
 					<div className='img-div'>
 						<img
@@ -173,18 +80,6 @@ export default class App extends Component {
 						/>
 					</div>
 				</div>
-				{/* <div>
-					<div className='card'>
-						<h3> {this.state.movie.title[num]}</h3>
-						<img src={this.state.movie.image[num]} />
-					</div>
-				</div> */}
-				<footer className='py-3 text-muted text-center'>
-					<span>
-						<img src='https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/20/000000/external-movie-cinema-icongeek26-linear-colour-icongeek26-7.png' />
-						Movie Something &copy; 2021
-					</span>
-				</footer>
 			</main>
 		);
 	}
--- src/movieComponent.jsx
+++ src/movieComponent.jsx
@@ -1,29 +1,71 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class MovieCards extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            movie: []
-         };
     }
-
-        axios
-        .get(`https://imdb-api.com/en/API/MostPopularMovies/${process.env.API_KEY}`)
-        .then(res => {
-            const num = Math.floor(Math.random() * 100);
-            const movie = res.data.items[num];
-            this.setState({ movie: movie});
-            console.log(movie);
-        })
     
     render() { 
         return (
-					<div>
-						<div className='card'>
-							<h3> {this.movies[0].title}</h3>
-							<img src={this.movies[0].img}/>
+					<div className='row mx-5 justify-content-between'>
+						<div className='card mb-3'>
+							<div className='row no-gutters'>
+								<div className='col-md-4'>
+									<img
+										// src={this.props.movieOption1.image}
+										alt='...'
+										width='150px'
+									/>
+								</div>
+								<div className='col-md-8'>
+									<div className='card-body'>
+										<h5 className='card-title'>
+											{this.props.movieOption1.title}
+										</h5>
+										<p className='card-text'>
+											Release Year: {this.props.movieOption1.year}
+										</p>
+										<p className='card-text'>
+											Director and Cast: {this.props.movieOption1.crew}
+										</p>
+										<p className='card-text'>
+											<small className='text-muted'>
+												Current Rank: {this.props.movieOption1.rank}
+											</small>
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className='card mb-3'>
+							<div className='row no-gutters'>
+								<div className='col-md-4'>
+									<img
+										// src={this.props.movieOption2.image}
+										alt='...'
+										width='150px'
+									/>
+								</div>
+								<div className='col-md-8'>
+									<div className='card-body'>
+										<h5 className='card-title'>
+											{this.props.movieOption2.title}
+										</h5>
+										<p className='card-text'>
+											Release Year: {this.props.movieOption2.year}
+										</p>
+										<p className='card-text'>
+											Director and Cast: {this.props.movieOption2.crew}
+										</p>
+										<p className='card-text'>
+											<small className='text-muted'>
+												Current Rank: {this.props.movieOption2.rank}
+											</small>
+										</p>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				);
